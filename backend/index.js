c1e9beb88a4f24086ec7bfc8b45f2144cc42a805
JarviS6051 <yashb150603@gmail.com>
2025-03-18 14:53:50 +0530

    Cors

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,35 @@
 const express = require('express');
-const dishesRoutes = require('./routes/dishesRoutes'); // Importing routes
+const dishesRoutes = require('./routes/dishesRoutes');
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
 
 const mongoose = require('mongoose');
 
+// MongoDB connection
 mongoose.connect('mongodb+srv://yashb150603:yashb150603@foodcommerce.nvmgy.mongodb.net/')
   .then(() => console.log('Connected!'));
 
 const app = express();
 const port = 5000;
-app.use(cors()); 
+
+// Allow requests only from the frontend (replace 'http://localhost:3000' with your frontend URL)
+const corsOptions = {
+  origin: 'https://ecommerce-food-app-main.vercel.app',  // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions)); // Apply the CORS options
+
 app.use(express.json()); // Correct usage
 
-// app.use((req,res,next)=>{
+// app.use((req, res, next) => {
 //   console.log("Time: ", Date.now());
 //   next();
 // });
 
 app.use('/api', dishesRoutes); // Using the routes
-
 app.use("/api", userRoutes);
-
-
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
